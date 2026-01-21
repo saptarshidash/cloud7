ea448b32c55e2d941beae77c3e17f264c06cf597
Saptarshi Das <saptarshi@Saptarshis-MacBook-Pro.local>
2026-01-21 22:40:14 +0530

    disabling csrf

--- cloud7-backend/src/main/java/com/cloud7/security/SecurityConfig.java
+++ cloud7-backend/src/main/java/com/cloud7/security/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> {})
+                .csrf(csrf -> csrf.disable())
 
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.disable())
